88eb171628d5e2d4482ee815da47619d0169853c
i0dheerajdileep <i.dheerajdileep@gmail.com>
2023-11-23 14:41:40 +0530

    loc changed 300

--- location.js
+++ location.js
@@ -52,7 +52,7 @@ function handleLocationUpdate(position) {
     const userLong = position.coords.longitude;
 
     if (locIndex !== -1 ) {
-        const thresholdDistance = 200; // Set your desired threshold distance
+        const thresholdDistance = 50; // Set your desired threshold distance
 
         const isNearLocation = isUserNearLocation(userLatt, userLong, locations[locIndex].x1, locations[locIndex].x2, thresholdDistance);
         console.log(isNearLocation,"isnearlocationnnnn")
